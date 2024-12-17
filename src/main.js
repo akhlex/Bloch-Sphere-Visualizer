2ca61005849437f17a3ef7490c912b8d8199b7f5
Akhil Alex <akhilalex003z@gmail.com>
2024-12-17 12:31:31 +0530

    fixed rotations
    THE THING FINALLY WORKS YIPPEEEEEEEEEEEE
    
    turns out explaining things to other people does help you understand it better

--- src/main.js
+++ src/main.js
@@ -15,7 +15,9 @@ document.body.appendChild( renderer.domElement );
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.enableDamping = true;
 
-camera.position.z = 4;
+camera.position.z = 3;
+camera.position.x = 2;
+camera.position.y = 1;
 
 //Sphere Definition  
 const sphereGeometry = new THREE.SphereGeometry( 1, 32, 32 );
@@ -28,18 +30,17 @@ sphere.position.set( 0, 0, 0 );
 //Vector Definition
 const vectorHead = new THREE.ConeGeometry( 0.1, 0.4 );
 vectorHead.translate( 0, 0.8, 0);
-const headMaterial = new THREE.MeshBasicMaterial( {color: 0x000000} );
+const headMaterial = new THREE.MeshBasicMaterial( {color: 0x2a2a69} );
 const vectorH = new THREE.Mesh( vectorHead, headMaterial );
 
 const vectorBody = new THREE.CylinderGeometry( 0.03, 0.03, 0.8 );
 vectorBody.translate( 0, 0.4, 0 );
-const bodyMaterial = new THREE.MeshBasicMaterial( {color: 0x000000} );
+const bodyMaterial = new THREE.MeshBasicMaterial( {color: 0x2a2a69} );
 const vectorB = new THREE.Mesh( vectorBody, bodyMaterial );
 
 const vector = new THREE.Group();
 vector.add( vectorH, vectorB );
 
-// const vector = new THREE.ArrowHelper( new THREE.Vector3( 0, -1, 0 ), new THREE.Vector3( 0, 0, 0 ), 1, 0xff00ff, 0.2, 0.1 );
 
 
 //Axes Definition
@@ -50,8 +51,7 @@ const axesN = new THREE.AxesHelper( -1 );
 axesN.setColors( 0x000000, 0x000000, 0x000000 );
 
 const axes = new THREE.Group();
-axes.add( axesP );
-axes.add( axesN );
+axes.add( axesP, axesN );
 
 
 
@@ -64,12 +64,12 @@ loader.load (
 
   function (droidFont) {
 
-    const zeroGeometry = new TextGeometry( '|0>', {depth: 0.1, size: 0.3, font: droidFont} );
+    const zeroGeometry = new TextGeometry( '|1>', {depth: 0.1, size: 0.3, font: droidFont} );
     const zeroMaterial = new THREE.MeshBasicMaterial( {color: 0x000000} );
     const zeroMesh = new THREE.Mesh( zeroGeometry, zeroMaterial );
     zeroMesh.position.set( -0.3, -1.5, 0);
 
-    const oneGeometry = new TextGeometry( '|1>', {depth: 0.1, size: 0.3, font: droidFont} );
+    const oneGeometry = new TextGeometry( '|0>', {depth: 0.1, size: 0.3, font: droidFont} );
     const oneMaterial = new THREE.MeshBasicMaterial( {color: 0x000000} );
     const oneMesh = new THREE.Mesh( oneGeometry, oneMaterial );
     oneMesh.position.set( -0.3, 1.35, 0 );
@@ -112,39 +112,37 @@ function animate() {
 renderer.setAnimationLoop( animate );
 
 
+
 //Gate Definitions
 export function x ( element ) {
-  element.addEventListener('click', () => { 
-    console.log( vector.rotation.x ); 
-    vector.rotateX( math.pi ) 
+  element.addEventListener('click', () => {  
+    vector.rotateOnWorldAxis( new THREE.Vector3( 1, 0, 0), math.pi ); 
   });
 }
 
 export function y ( element ) {
   element.addEventListener('click', () => { 
-    console.log( vector.rotation.z ); 
-    vector.rotateZ( math.pi ) 
+    vector.rotateOnWorldAxis( new THREE.Vector3( 0, 0, 1 ), math.pi );
   });
 }
 
 export function z ( element ) {
   element.addEventListener('click', () => { 
-    console.log( vector.rotation.y ); 
-    vector.rotateY( math.pi ) 
+    vector.rotateOnWorldAxis( new THREE.Vector3( 0, 1, 0 ), math.pi ); 
   });
 }
 
 export function h ( element ) {
   element.addEventListener('click', () => { 
-    console.log( "Old X: ", vector.rotation.x, "Old Y: ", vector.rotation.y); 
-    vector.rotateX( math.pi ); 
-    vector.rotateY( math.pi / 2 )
+    vector.rotateOnWorldAxis( new THREE.Vector3( 1, 0, 0 ), math.pi );
+    vector.rotateOnWorldAxis( new THREE.Vector3( 0, 0, 1 ), math.pi / 2 );
    });
 }
 
+//Resets the scene
 export function reset ( element ) {
   element.addEventListener('click', () => { 
-    console.clear(); 
-    vector.rotation.set(0, 0, 0) 
+    vector.rotation.set( 0, 0, 0 );
+    console.clear();  
   });
 }
